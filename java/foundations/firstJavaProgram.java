c6bb3e533acda8a3b0c2ff4b541a4ab9cedf999b
Jonathan Pacheco <jp.pacheco19@gmail.com>
2022-06-07 15:56:18 -0400

    confirm

---
+++ java/foundations/firstJavaProgram.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+
+public class firstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My Name is JP Pacheco");
+        System.out.println("I am 21 years old");
+        System.out.println("My hometown is Fairfax, VA");
+    }
+}
