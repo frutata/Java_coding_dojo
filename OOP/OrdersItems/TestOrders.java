7a6596d0c2883ec872709cb214ff58d6535b0d35
Jonathan Pacheco <jp.pacheco19@gmail.com>
2022-06-09 15:00:10 -0400

    Barista Challenge

--- OOP/OrdersItems/TestOrders.java
+++ OOP/OrdersItems/TestOrders.java
@@ -1,73 +1,50 @@
 import java.util.ArrayList;
+
 public class TestOrders {
     public static void main(String[] args) {
     
-        Order testOrder = new Order();
-        Item testItem = new Item();
+        //menu items
+        Item item1 = new Item("Cappucino", 4.0);
+        Item item2 = new Item("Mocha", 7.0);
+        Item item3 = new Item("Latte", 6.5);
+        Item item4 = new Item("Drip Coffee", 5.0);
 
-        // Menu items
-        Item item1 = new Item();
-        item1.name = "Mocha";
-        item1.price = 3.5;
-
-        Item item2 = new Item();
-        item2.name = "Drip Coffee";
-        item2.price = 2.0;
-
-        Item item3 = new Item();
-        item3.name = "Latte";
-        item3.price = 4.0;
-
-        Item item4 = new Item();
-        item4.name = "Cappuccino";
-        item4.price = 2.5;
-    
-        // Order variables -- order1, order2 etc.
+        //orders for unspecified guests
         Order order1 = new Order();
-        order1.name = "JP";
-
         Order order2 = new Order();
-        order2.name = "Kelly";
 
-        Order order3 = new Order();
-        order3.name = "Tyler";
+        //orders for specific guests
+        Order order3 = new Order("JP");
+        Order order4 = new Order("Kelly");
+        Order order5 = new Order("Tyler");
 
-        Order order4 = new Order();
-        order4.name = "Austin";
-    
-        // Application Simulations
-        order2.items.add(item1);
-        order2.total += item1.price;
+        //adding items to orders
+        order1.addItems(item1);
+        order1.addItems(item3);
 
-        order3.items.add(item4);
-        order3.total += item4.price;
+        order2.addItems(item2);
+        order2.addItems(item3);
 
-        order4.items.add(item3);
-        order4.total += item3.price;
+        order3.addItems(item4);
+        order3.addItems(item1);
 
-        order1.ready = true;
+        order4.addItems(item3);
+        order4.addItems(item4);
 
-        order4.items.add(item3);
-        order4.items.add(item3);
-        order4.total += item3.price *2;
+        order5.addItems(item1);
+        order5.addItems(item2);
 
-        order2.ready = true;
+        //setting order 1 ready to true
+        order3.setReady(true);
 
-        // Use this example code to test various orders' updates
-        // System.out.printf("Name: %s\n", order1.name);
-        // System.out.printf("Total: %s\n", order1.total);
-        // System.out.printf("Ready: %s\n", order1.ready);
+        //checking the status of the first two orders
+        System.out.println(order1.getStatusMessage());
+        System.out.println(order3.getStatusMessage());
+        
+        //testing the getOrderTotal method
+        System.out.println(order3.getOrderTotal());
 
-        System.out.printf("Name: %s\n", order2.name);
-        System.out.printf("Total: %s\n", order2.total);
-        System.out.printf("Ready: %s\n", order2.ready);
-
-        System.out.printf("Name: %s\n", order3.name);
-        System.out.printf("Total: %s\n", order3.total);
-        System.out.printf("Ready: %s\n", order3.ready);
-
-        System.out.printf("Name: %s\n", order4.name);
-        System.out.printf("Total: %s\n", order4.total);
-        System.out.printf("Ready: %s\n", order4.ready);
+        //testing the display method
+        order3.display();
     }
 }
